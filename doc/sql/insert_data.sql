6ad904576b6b11ee6fca70eca191b487c370627d
sjklan4 <dpeltms4@gmail.com>
2023-04-10 18:11:30 +0900

    0410게시판

--- doc/sql/insert_data.sql
+++ doc/sql/insert_data.sql
@@ -2,16 +2,26 @@
 -- INSERT INTO board_info(board_title, board_contents, board_write_date)
 -- VALUES('제목 1', '내용 1', NOW());
 
--- INSERT INTO board_info(board_title, board_contents, board_write_date)
--- VALUES('제목 14', '내용 14', NOW())
--- ,('제목 15', '내용 15', NOW())
--- ,('제목 16', '내용 16', NOW())
--- ,('제목 17', '내용 17', NOW())
--- ,('제목 18', '내용 18', NOW())
--- ,('제목 19', '내용 19', NOW());
+INSERT INTO board_info(board_title, board_contents, board_write_date)
+VALUES('제목 14', '내용 14', NOW())
+,('제목 22', '내용 22', NOW())
+,('제목 23', '내용 23', NOW())
+,('제목 24', '내용 24', NOW())
+,('제목 25', '내용 25', NOW())
+,('제목 26', '내용 26', NOW());
 
 -- 
 -- 
-SELECT * FROM board_info;
 
-COMMIT;
+-- 
+-- COMMIT;
+-- SELECT board_no
+-- 			,board_title
+-- 			,board_write_date 
+-- FROM board_info
+-- WHERE board_del_flg = '0'
+-- ORDER BY board_no ASC
+-- LIMIT 5 OFFSET 0;
+-- COMMIT;
+
+-- SELECT COUNT(*) FROM board_info;
